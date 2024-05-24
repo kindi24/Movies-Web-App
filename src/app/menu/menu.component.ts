ecefa633b58f745cd4ae72ffdd7632ef869fcd85
kindi24 <kyndelerosvasilis@gmail.com>
2024-05-24 12:18:00 +0300

    Header and Footer

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
