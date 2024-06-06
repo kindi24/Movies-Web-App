0e10c9efdaeebf607744c10086bcace7f4ca9038
kindi24 <kyndelerosvasilis@gmail.com>
2024-06-06 15:08:50 +0300

    Update Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  
+  constructor(private router: Router) {}
+
+  logout(){
+    const confirmation = confirm('Are you sure you want to log out?');
+    if (confirmation) {
+      alert('Successful log-out');
+      this.router.navigate(['']);
+    }
+  }
 }
