c9dd8ef0f4202dcc414a4bd1b34d482284e2e4fa
kindi24 <kyndelerosvasilis@gmail.com>
2024-06-10 09:50:36 +0300

    Inital commit

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
-import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
