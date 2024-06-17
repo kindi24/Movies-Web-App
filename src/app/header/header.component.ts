ab66158ab0f9f989ef2701187bf86ce1b266f5e9
kindi24 <kyndelerosvasilis@gmail.com>
2024-06-17 09:44:00 +0300

    Create dropdown menu in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   constructor(private router: Router) {}
+
+  profilePhoto: any = "assets/images/person.svg";
+  profileVisible = false;
+
+  profileContent(){
+    this.profileVisible = !this.profileVisible;
+  }
+
+  myAccount(){
+    console.log("My account");
+  }
 
   logout(){
     const confirmation = confirm('Are you sure you want to log out?');
