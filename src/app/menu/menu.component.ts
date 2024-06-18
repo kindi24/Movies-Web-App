13bf578a32b956e52fd6870a375c0c3e2cb8baec
kindi24 <kyndelerosvasilis@gmail.com>
2024-06-18 10:13:27 +0300

    Get input value

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,22 +3,24 @@ import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { OmdbServiceComponent } from '../omdb-service/omdb-service.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, OmdbServiceComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, OmdbServiceComponent, FormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
   checkWidth = window.innerWidth;
 
-  searchMovies(){
-    console.log("Movies search");
-  }
-
+  movieTitle: string = '';
   movieData: any;
+
+  searchMovies(){
+    console.log("Movies search: " +this.movieTitle);
+  }
 
   /*
   constructor(private omdbService: omdbService) {}
