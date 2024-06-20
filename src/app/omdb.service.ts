ad8314f7b7f092587919422cfa9c09c349b135ec
kindi24 <kyndelerosvasilis@gmail.com>
2024-06-20 11:16:36 +0300

    Update omdb.service.ts

--- src/app/omdb.service.ts
+++ src/app/omdb.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class OmdbService {
+export class OmdbService implements OnInit{
 
   private apiUrl = 'http://www.omdbapi.com/?apikey=826f752f';
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      
+  }
 
   searchMoviesByTitle(title: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}&s=${title}`);
